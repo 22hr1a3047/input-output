4dfe064ac70bf038e1f97fd6761c9e084c585a1f
22hr1a3047 <22hr1a3047@gmail.com>
2024-09-18 16:07:05 +0530

    Update input.py

--- input.py
+++ input.py
@@ -32,3 +32,41 @@ For Banerjee's question "Will you be a wicketkeeper?" Dhoni's confident reply wa
 
 #code for second question
 code:-
+p=input("Banerjee's Question:\n")
+r=input("Dhoni's Reply:\n")
+print("for Banerjee's question:",p,"Dhoni's confident reply was",r)
+
+'''
+3)It was in the 1997-98 season that Ranchi saw the rise of the Captain Cool in the interschool trophy between DAV Jawahar Vidhya Mandir and Kendriya School. It was in that match Dhoni convinced Banerjee to be the opener and justified it with a double century.
+Write a program to display the details of the match with Team name, Scores of the team and Overs played.
+Input and Output Format:  
+Refer sample input and output for formatting specifications.
+[All text in bold corresponds to input and the rest corresponds to output]
+Sample Input and Output:
+Team 1:
+Team Name:
+DAV Jawahar Vidhya Mandir
+Score:
+150
+Overs played:
+20
+Team 2:
+Team name:
+Kendriya School
+Score:
+110
+Overs played:
+18
+Match Details:
+Team 1:
+Name: DAV Jawahar Vidhya Mandir
+Score: 150
+Overs played: 20
+Team 2:
+Name:  Kendriya School
+Score: 110
+Overs played: 18
+'''
+#code for third program
+
+code:-
